b89762609f21acdcddf0899dc85ee39d6cbee17c
Andrew Walsh <andrew.jordan.walsh@gmail.com>
2021-05-22 17:39:30 -0500

    refactored to businesses and employees

--- server.js
+++ server.js
@@ -36,29 +36,31 @@ var con = mysql.createPool({
   // mysql://b9e533a14b394b:0d3e9617@us-cdbr-east-03.cleardb.com/heroku_5eadd1519f813b4?reconnect=true
   
 //confirm connection  
-    let sql = "CREATE TABLE IF NOT EXISTS `posts` ( `post_id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key', `post_time` datetime DEFAULT CURRENT_TIMESTAMP, `post_type` enum('Sell','Buy') NOT NULL, `price` decimal(20,2) DEFAULT NULL, `post_amount` varchar(255) DEFAULT NULL, `post_description` text, `post_image_url` varchar(255) DEFAULT NULL, `post_location` varchar(255) DEFAULT NULL, `user_id` int unsigned NOT NULL, `post_name` varchar(255) NOT NULL, PRIMARY KEY (`post_id`), KEY `user_id` (`user_id`), FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) )"
+    let sql = "CREATE TABLE IF NOT EXISTS `posts` ( `post_id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key', `post_time` datetime DEFAULT CURRENT_TIMESTAMP, `post_type` enum('Sell','Buy') NOT NULL, `price` decimal(20,2) DEFAULT NULL, `post_amount` varchar(255) DEFAULT NULL, `post_description` text, `post_image_url` varchar(255) DEFAULT NULL, `post_location` varchar(255) DEFAULT NULL, `employee_id` int unsigned NOT NULL, `post_name` varchar(255) NOT NULL, PRIMARY KEY (`post_id`), KEY `employee_id` (`employee_id`), FOREIGN KEY (`employee_id`) REFERENCES `employees` (`employee_id`) )"
     con.query(sql, function (err, result) {
       if (err) console.error(err);
     });
-    sql = "CREATE TABLE IF NOT EXISTS `teams` ( `team_id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key', `team_name` varchar(255) NOT NULL, `team_categories` varchar(255) NOT NULL, `team_description` text, `team_image_url` varchar(255) DEFAULT NULL, PRIMARY KEY (`team_id`) )";
+    sql = "CREATE TABLE IF NOT EXISTS `businesses` ( `business_id` int NOT NULL AUTO_INCREMENT COMMENT 'primary key', `business_name` varchar(255) NOT NULL, `business_categories` varchar(255) NOT NULL, `business_description` text, `business_image_url` varchar(255) DEFAULT NULL, PRIMARY KEY (`business_id`) )";
     con.query(sql, function (err, result) {
       if (err) console.error(err);
     });
-    sql = "CREATE TABLE IF NOT EXISTS `users` ( `user_id` int unsigned NOT NULL AUTO_INCREMENT, `first_name` varchar(63) NOT NULL, `last_name` varchar(63) NOT NULL, `email` varchar(127) NOT NULL, `password` varchar(255) NOT NULL, `profile_image_url` varchar(255) DEFAULT NULL, `team_id` int DEFAULT NULL, PRIMARY KEY (`user_id`), UNIQUE KEY `email` (`email`), KEY `team_id` (`team_id`), FOREIGN KEY (`team_id`) REFERENCES `teams` (`team_id`) )";
+    sql = "CREATE TABLE IF NOT EXISTS `employees` ( `employee_id` int unsigned NOT NULL AUTO_INCREMENT, `first_name` varchar(63) NOT NULL, `last_name` varchar(63) NOT NULL, `email` varchar(127) NOT NULL, `password` varchar(255) NOT NULL, `profile_image_url` varchar(255) DEFAULT NULL, `business_id` int DEFAULT NULL, PRIMARY KEY (`employee_id`), UNIQUE KEY `email` (`email`), KEY `business_id` (`business_id`), FOREIGN KEY (`business_id`) REFERENCES `businesses` (`business_id`) )";
     con.query(sql, function (err, result) {
       if (err) console.error(err);
     });
 
 
 app.get('/', (req, res) => {
-  res.write("Hello!");
+  res.write("Welcome to the Veteran API!");
+  res.write(DOCUMENTATION_STR);
   res.end();
 });
-/*-------START OF TEAMS API-------*/
 
-//get all teams
-app.get('/teams', (req, res) => {
-  var sql = "SELECT * FROM teams";
+/*-------START OF BUSINESSES API-------*/
+
+//get all businesses
+app.get('/businesses', (req, res) => {
+  var sql = "SELECT * FROM businesses";
   con.query(sql, function (err, result) {
     if (err) console.error(err);
     res.json(result)
@@ -66,56 +68,57 @@ app.get('/teams', (req, res) => {
   });
 })
 
-//get one team by id
-app.get('/teams/:id', (req, res) => {
+//get one businesses by id
+app.get('/businesses/:id', (req, res) => {
   let url = req.url;
   let arr = url.split("/");
   let id = arr[arr.length - 1];
-  var sql = "SELECT * FROM teams WHERE team_id = ?";
+  var sql = "SELECT * FROM businesses WHERE business_id = ?";
   con.query(sql, [id], function (err, result) {
     if (err) console.error(err);
     res.json(result);
   });
 })
 
-//create a team
-app.post('/teams/new', (req, res) => {
+//create a business
+app.post('/businesses/new', (req, res) => {
   let name = req.body.name;
   let categories = req.body.categories;
   let description = req.body.description;
   let imageURL = req.body.imageURL;
   console.log(name, categories, description);
-  var sql = "INSERT INTO teams (team_name, team_categories, team_description, team_image_url) VALUES (?, ?, ?, ?)";
+  var sql = "INSERT INTO businesses (business_name, business_categories, business_description, business_image_url) VALUES (?, ?, ?, ?)";
   con.query(sql, [name, categories, description, imageURL], function (err, result) {
     if (err) console.error(err);
-    res.send("Created new team!");
+    res.send("Created new business!");
   });
 })
 
-//update team
-app.put('/teams/:id', (req, res) => {
+//update business
+app.put('/businesses/:id', (req, res) => {
   let url = req.url;
   let arr = url.split("/");
   let id = arr[arr.length - 1];
   let name = req.body.name;
   let categories = req.body.categories;
   let description = req.body.description;
-  var sql = "REPLACE INTO teams (team_id), team_name, team_categories, team_description) VALUES (?, ?, ?, ?)";
-  con.query(sql, [id, name, categories, description], function (err, result) {
+  let imageURL = req.body.imageURL;
+  var sql = "UPDATE businesses SET business_name = ?, business_categories = ?, business_categories = ?, business_image_url = ? WHERE business_id = ?";
+  con.query(sql, [name, categories, description, imageURL, id], function (err, result) {
     if (err) console.error(err);
-    res.send("Edited team!");
+    res.send("Edited business!");
   });
 })
 
-//delete team
-app.delete('/teams/:id', (req, res) => {
+//delete business
+app.delete('/businesses/:id', (req, res) => {
   let url = req.url;
   let arr = url.split("/");
   let id = arr[arr.length - 1];
-  var sql = "DELETE FROM teams WHERE team_id = ?";
+  var sql = "DELETE FROM businesses WHERE business_id = ?";
   con.query(sql, id, function (err, result) {
     if (err) console.error(err);
-    res.send("Deleted team!");
+    res.send("Deleted business!");
   });
 })
 
@@ -151,10 +154,10 @@ app.post('/posts/new', (req, res) => {
   let imageURL = req.body.imageURL;
   let location = req.body.location;
   let name = req.body.name;
-  let userID = req.body.userID;
+  let employeeID = req.body.employeeID;
 
-  var sql = "INSERT INTO posts (post_type, price, post_amount, post_description, post_image_url, post_location, post_name, user_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-  con.query(sql, [type, price, amount, description, imageURL, location, name, userID], function (err, result) {
+  var sql = "INSERT INTO posts (post_type, price, post_amount, post_description, post_image_url, post_location, post_name, employee_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+  con.query(sql, [type, price, amount, description, imageURL, location, name, employeeID], function (err, result) {
     if (err) console.error(err);
     res.send("Created new post!");
   });
@@ -169,10 +172,9 @@ app.put('/posts/:id', (req, res) => {
   let imageURL = req.body.imageURL;
   let location = req.body.location;
   let name = req.body.name;
-  let userID = req.body.userID;
 
-  var sql = "INSERT INTO posts (post_type, price, post_amount, post_description, post_image_url, post_location, post_name) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
-  con.query(sql, [type, price, amount, description, imageURL, location, name, userID], function (err, result) {
+  var sql = "UPDATE posts SET post_type = ?, price = ?, post_amount = ?, post_description = ?, post_image = ?, post_location = ?, post_name = ?";
+  con.query(sql, [type, price, amount, description, imageURL, location, name], function (err, result) {
     if (err) console.error(err);
     res.send("Edited post!");
   });
@@ -190,70 +192,219 @@ app.delete('/posts/:id', (req, res) => {
   });
 })
 
-/*-------START OF USERS API-------*/
+/*-------START OF EMPLOYEES API-------*/
 
-//get all users
-app.get('/users', (req, res) => {
-  var sql = "SELECT * FROM users";
+//get all employees
+app.get('/employees', (req, res) => {
+  var sql = "SELECT * FROM employees";
   con.query(sql, function (err, result) {
     if (err) console.error(err);
     res.json(result);
   });
 })
 
-//get one user by id
-app.get('/users/:id', (req, res) => {
+//get one employee by id
+app.get('/employees/:id', (req, res) => {
   let url = req.url;
   let arr = url.split("/");
   let id = arr[arr.length - 1];
-  var sql = "SELECT * FROM users WHERE user_id = ?";
+  var sql = "SELECT * FROM employees WHERE employee_id = ?";
   con.query(sql, [id], function (err, result) {
     if (err) console.error(err);
     res.json(result);
   });
 })
 
-//create a user
-app.post('/users/new', (req, res) => {
+//create an employee
+app.post('/employees/new', (req, res) => {
   let firstName = req.body.firstName;
   let lastName = req.body.lastName;
   let email = req.body.email;
   let password = req.body.password;
   let imageURL = req.body.imageURL;
-  let teamID = req.body.teamID;
+  let businessID = req.body.businessID;
 
-  var sql = "INSERT INTO users (first_name, last_name, email, password, profile_image_url, team_id) VALUES (?, ?, ?, ?, ?, ?)";
-  con.query(sql, [firstName, lastName, email, password, imageURL, teamID], function (err, result) {
+  var sql = "INSERT INTO employees (first_name, last_name, email, password, profile_image_url, business_id) VALUES (?, ?, ?, ?, ?, ?)";
+  con.query(sql, [firstName, lastName, email, password, imageURL, businessID], function (err, result) {
     if (err) console.error(err);
-    res.send("Created new user!");
+    res.send("Created new employee!");
   });
 })
 
-//update a user
-app.post('/users/new', (req, res) => {
+//update an employee
+app.post('/employees/new', (req, res) => {
   let firstName = req.body.firstName;
   let lastName = req.body.lastName;
   let email = req.body.email;
   let password = req.body.password;
   let imageURL = req.body.imageURL;
-  let teamID = req.body.teamID;
+  let businessID = req.body.businessID;
 
-  var sql = "INSERT INTO users (first_name, last_name, email, password, profile_image_url, team_id) VALUES (?, ?, ?, ?, ?, ?)";
-  con.query(sql, [firstName, lastName, email, password, imageURL, teamID], function (err, result) {
+  var sql = "INSERT INTO employees (first_name, last_name, email, password, profile_image_url, business_id) VALUES (?, ?, ?, ?, ?, ?)";
+  con.query(sql, [firstName, lastName, email, password, imageURL, businessID], function (err, result) {
     if (err) console.error(err);
-    res.send("Updated user!");
+    res.send("Updated employee!");
   });
 })
 
-//delete user
-app.delete('/users/:id', (req, res) => {
+//delete employee
+app.delete('/employees/:id', (req, res) => {
   let url = req.url;
   let arr = url.split("/");
   let id = arr[arr.length - 1];
-  var sql = "DELETE FROM posts WHERE user_id = ?";
+  var sql = "DELETE FROM posts WHERE employee_id = ?";
   con.query(sql, id, function (err, result) {
     if (err) console.error(err);
-    res.send("Deleted user!");
+    res.send("Deleted employee!");
   });
 })
+
+
+const DOCUMENTATION_STR = ```DOCUMENTATION FOR THE VETERAN APP API
+
+BELOW ARE THE THREE TABLES AND THE URLS THAT REQUESTS NEED TO BE SENT TO IN ORDER TO INTERACT WITH THE DATABASE:
+
+TO POST INFO, YOU NEED TO SEND AN OBJECT WITH KEY-VALUE PAIRS IN THE "DATA" FIELD OF THE JSON OBJECT YOU SEND!!!!
+----------------------------------------------------------------------------------------------
+
+EMPLOYEES API:
+
+TO GET ALL EMPLOYEES
+GET https://vet-codeathon.herokuapp.com/employees/
+
+------------------------------------------------------------------------------------------------
+
+TO GET AN EMPLOYEE BY ID
+GET https://vet-codeathon.herokuapp.com/employees/${id}
+
+---------------------------------------------------------------------------------------------------
+
+TO MAKE A NEW EMPLOYEE
+POST https://vet-codeathon.herokuapp.com/employees/new
+
+^^^NEEDS THESE ATTRIBUTES IN BODY:
+{
+    firstName:
+    lastName:
+    email:
+    password:
+    imageURL <OPTIONAL>:
+    businessID <OPTIONAL>:
+}
+
+-------------------------------------------------------------------------------
+
+TO UPDATE AN EMPLOYEE
+PUT https://vet-codeathon.herokuapp.com/employees/${id}
+
+^^^NEEDS THESE ATTRIBUTES IN BODY:
+{
+    firstName:
+    lastName:
+    email : !!!THIS IS UNIQUE, ONE ACCOUNT PER EMAIL, THE DATABASE REJECT DUPLICATES!!!
+    password:
+    imageURL <OPTIONAL>:
+    businessID <OPTIONAL>:
+}
+
+----------------------------------------------------------
+
+TO DELETE AN EMPLOYEE
+DELETE https://vet-codeathon.herokuapp.com/employees/${id}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+BUSINESSES API:
+
+TO GET ALL BUSINESSES
+GET https://vet-codeathon.herokuapp.com/businesses
+
+--------------------------------------------------------------------------------------
+
+TO GET A BUSINESS BY ID
+GET https://vet-codeathon.herokuapp.com/businesses/${id}
+
+--------------------------------------------------------------------------------------------
+
+TO MAKE A NEW BUSINESS
+POST https://vet-codeathon.herokuapp.com/businesses/new
+
+^^^THIS NEEDS THESE ATTRIBUTES IN BODY:
+{
+    name:
+    categories:
+    description <OPTIONAL>:
+    imageURL <OPTIONAL>:
+}
+
+---------------------------------------------------------------------
+
+TO UPDATE A BUSINESS
+PUT https://vet-codeathon.herokuapp.com/businesses/${id}
+
+^^^NEEDS THESE ATTRIBUTES IN BODY:
+{
+    name:
+    categories:
+    description <OPTIONAL>:
+    imageURL <OPTIONAL>:
+}
+
+-----------------------------------------------------------------------------------
+
+TO DELETE A BUSINESS
+DELETE https://vet-codeathon.herokuapp.com/businesses/${id}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+POSTS API:
+
+TO GET ALL POSTS
+GET https://vet-codeathon.herokuapp.com/posts
+
+------------------------------------------------------------------------------------------------
+
+TO GET A POST BY ID
+GET https://vet-codeathon.herokuapp.com/posts/${id}
+
+---------------------------------------------------------------------------------------------------
+
+TO MAKE A NEW POST
+POST https://vet-codeathon.herokuapp.com/posts/new
+
+^^^NEEDS THESE ATTRIBUTES IN BODY:
+{
+    type: !!! HAS TO BE "BUY" OR "SELL" !!!
+    price <OPTIONAL>:
+    amount <OPTIONAL>:
+    description <OPTIONAL>:
+    imageURL <OPTIONAL>:
+    location <OPTIONAL>:
+    employeeID:
+    name: 
+}
+
+-------------------------------------------------------------------------------
+
+TO UPDATE A POST
+PUT https://vet-codeathon.herokuapp.com/posts/${id}
+
+^^^NEEDS THESE ATTRIBUTES IN BODY:
+{
+    type: !!! HAS TO BE "BUY" OR "SELL" !!!
+    price <OPTIONAL>:
+    amount <OPTIONAL>:
+    description <OPTIONAL>:
+    imageURL <OPTIONAL>:
+    location <OPTIONAL>:
+    name: 
+}
+
+----------------------------------------------------------
+
+TO DELETE A POST
+DELETE https://vet-codeathon.herokuapp.com/posts/${id}
+
+ ```
 
